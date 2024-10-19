6001351316d013089df7ed1973ba0acc51dc065c
Prithvi <prithvin19@gmail.com>
2024-10-19 15:09:15 +0530

    changes

--- components/Team.jsx
+++ components/Team.jsx
@@ -6,7 +6,7 @@ import { FaArrowRightLong } from "react-icons/fa6";
 const Team = () => {
     return (
         <section className="flex flex-col items-center justify-center gap-5 md:gap-8 py-7 lg:py-12 md:flex-row font-merriWeather">
-            <Link href={"/team"} className="relative w-[320px] md:w-[460px] md:h-[620px] h-[440px] overflow-hidden group z-0 contrast-100 bg-white border-2 border-info">
+            <Link href={"/team/founder"} className="relative w-[320px] md:w-[460px] md:h-[620px] h-[440px] overflow-hidden group z-0 contrast-100 bg-white border-2 border-info">
                 <Image alt="sample-image" src={"/daga-1.png"} fill className="object-contain object-right transition-all duration-200 ease-in-out group-hover:scale-110 scale-105" />
                 <div className="absolute bottom-0 left-0 z-10 flex items-center justify-center gap-6 md:gap-16 w-full py-2 px-5 lg:px-8 backdrop-brightness-[0.60]">
                     <div className="block">
@@ -18,7 +18,7 @@ const Team = () => {
                     </div>
                 </div>
             </Link>
-            <Link href={"/team"} className="relative w-[320px] md:w-[460px] md:h-[620px] h-[440px] overflow-hidden group z-0 contrast-100 bg-white border-2 border-info">
+            <Link href={"/team/litigations"} className="relative w-[320px] md:w-[460px] md:h-[620px] h-[440px] overflow-hidden group z-0 contrast-100 bg-white border-2 border-info">
                 <Image alt="sample-image" src={"/daga-2.png"} fill className="object-contain object-left transition-all duration-200 ease-in-out group-hover:scale-110 scale-105" />
                 <div className="absolute bottom-0 left-0 z-10 flex items-center justify-center gap-6 xl:gap-16 w-full py-2 px-5 lg:px-8 backdrop-brightness-[0.60]">
                     <div className="block">
