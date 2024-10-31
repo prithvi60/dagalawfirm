9447b4117179d5bce58f46a3dab5cea067009edb
Prithvi <prithvin19@gmail.com>
2024-10-31 08:41:13 +0530

    scheme

--- app/page.js
+++ app/page.js
@@ -6,9 +6,47 @@ import Hero from "@/components/Hero";
 import Modal from "@/components/Modal";
 import PracticesComponent from "@/components/PracticesComponent";
 import Team from "@/components/Team";
-
+import Head from 'next/head';
 export default function Home() {
   return (
+    <>
+    <Head>
+        <script type="application/ld+json">
+            {`
+            {
+              "@context": "https://schema.org",
+              "@type": "LawFirm",
+              "name": "Daga & Daga Intellectual Property Attorneys",
+              "url": "https://dagaanddaga.com/",
+              "logo": "https://dagaanddaga.com/daga-new-logo.png",
+              "description": "Trusted legal support for your important matters. Here to guide and protect you every step of the way.",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Swarna Shree Complex, 3rd Floor, 36, Veerappan St",
+                "addressLocality": "Sowcarpet",
+                "addressRegion": "Tamil Nadu, Chennai",
+                "postalCode": "600001",
+                "addressCountry": "IN"
+              },
+              "contactPoint": {
+                "@type": "ContactPoint",
+                "telephone": "+91-9551028280",
+                "contactType": "Customer Service",
+                "areaServed": "IN",
+                "availableLanguage": "English"
+              },
+              "sameAs": [
+                "https://www.linkedin.com/company/daga-daga"
+              ],
+              "department": {
+                "@type": "LegalService",
+                "name": "Intellectual Property Law",
+                "serviceType": "IPR Services"
+              }
+            }
+            `}
+        </script>
+    </Head>
     <main>
       <Hero />
       <FirmIntro />
@@ -19,5 +57,6 @@ export default function Home() {
       <Contact />
       <Modal />
     </main>
+    </>
   );
 }
