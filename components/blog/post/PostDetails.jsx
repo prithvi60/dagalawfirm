84dbed0a13c30234d09b7ede64f45917f8f22bcf
GOGA-23 <gokulgandhi97@gmail.com>
2024-12-17 10:43:09 +0530

    bug fixed

--- components/blog/post/PostDetails.jsx
+++ components/blog/post/PostDetails.jsx
@@ -22,13 +22,26 @@ const PostDetails = ({ post }) => {
                 animate="animate"
                 className="max-w-7xl prose prose-blue mx-auto prose-h1:text-3.5xl prose-h1:font-bold prose-h2:text-2xl prose-h2:font-bold prose-h3:text-2xl prose-h3:font-bold prose-h4:text-2xl prose-h4:font-bold prose-p:leading-relaxed prose-p:text-lg lg:prose-p:text-xl prose-p:mt-4 prose-p:mb-4 prose-blockquote:leading-relaxed prose-blockquote:text-lg lg:prose-blockquote:text-xl prose-list:list-disc prose-list-decimal"
             >
-                <PortableText value={post.body} />
+                <PortableText value={post.body} components={customComponents} />
             </motion.div>
         </section>
     );
 };
 
 export default PostDetails;
+
+const customComponents = {
+    types: {
+        image: ({ value }) => (
+            // eslint-disable-next-line @next/next/no-img-element
+            <img
+                src={value?.imageUrl} // Use the resolved `imageUrl` from GROQ
+                alt={value?.alt || "Image"}
+                className="w-full h-full max-w-md max-h-full mx-auto mt-12 mb-12 md:mt-20 md:mb-10"
+            />
+        ),
+    },
+};
 
 // const customComponents = {
 //     block: {
